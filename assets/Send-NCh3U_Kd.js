bbcca0e6e0e010f140198d72dfd129c8cfc07dca
saipriyasahoo228 <saipriyasahoo228@gmail.com>
2025-02-07 15:39:30 +0530

    Add files via upload

---
+++ assets/Send-NCh3U_Kd.js
@@ -0,0 +1,1 @@
+import{a5 as s,j as t}from"./index-IJTRt1jI.js";const c=s(t.jsx("path",{d:"M19 8H5c-1.66 0-3 1.34-3 3v6h4v4h12v-4h4v-6c0-1.66-1.34-3-3-3m-3 11H8v-5h8zm3-7c-.55 0-1-.45-1-1s.45-1 1-1 1 .45 1 1-.45 1-1 1m-1-9H6v4h12z"}),"Print"),o=s(t.jsx("path",{d:"M2.01 21 23 12 2.01 3 2 10l15 2-15 2z"}),"Send");export{c as P,o as S};
