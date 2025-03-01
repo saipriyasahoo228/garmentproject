3304223869519056638830c454b89837d856115e
saipriyasahoo228 <saipriyasahoo228@gmail.com>
2025-03-01 13:03:29 +0530

    commit

--- src/constant.js
+++ src/constant.js
@@ -1,5 +1,5 @@
-let API_URI = 'http://127.0.0.1:8000/';
+let API_URI = 'https://garment002.pythonanywhere.com/';
 if (process.env.NODE_ENV === 'development') {
-  API_URI = 'http://127.0.0.1:8000/';
+  API_URI = 'https://garment002.pythonanywhere.com/';
 }
 export const API_BASE_URL = API_URI;
